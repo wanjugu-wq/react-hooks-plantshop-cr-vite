c23872ea8fe59a9a5e9b50837159d26d77652b09
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-14 13:41:10 +0300

    added page load feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const API = "http://localhost:6001/plants";
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} />
     </div>
   );
 }
