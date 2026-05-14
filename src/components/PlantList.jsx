266883b8275209d877cbacad6f0f34b275292911
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-14 15:02:27 +0300

    added search filter

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList({ plants }) {
+function PlantList({ plants}) {
   return (
     <ul className="cards">
       {plants.map((plant) => (
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage({ plants, setPlants }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearch(e) {
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
   return (
     <main>
       <NewPlantForm plant={plants} setPlant={setPlants} />
-      <Search />
-      <PlantList plants={plants} />
+      <Search setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
